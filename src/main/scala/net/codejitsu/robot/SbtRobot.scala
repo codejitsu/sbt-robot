9ca6628ff5f771216bc24c8ac7801958873f41f5
Alex <me@codejitsu.net>
2015-06-03 06:44:13 +0200

    Added 'defineTask' method for user specific tasks.

--- src/main/scala/net/codejitsu/robot/SbtRobot.scala
+++ src/main/scala/net/codejitsu/robot/SbtRobot.scala
@@ -1,22 +1,45 @@
 package net.codejitsu.robot
 
-import net.codejitsu.tasks.dsl.{GenericTask, TaskM, User, NoUser}
-import sbt.AutoPlugin
+import net.codejitsu.tasks.dsl.TaskM
+import net.codejitsu.tasks.dsl.VerbosityLevel._
 import sbt._
-import Keys._
-import sbt.complete.Parsers._
+
+import scala.util.Success
 
 /**
  * Sbt Robot plugin main class
  */
-class SbtRobot extends AutoPlugin {
-  val user = settingKey[User]("user")
-  val tasks = settingKey[Seq[GenericTask]]("tasks")
+object SbtRobot extends sbt.AutoPlugin {
+  object autoImport {
+    /**
+     * Configuration for the plugin.
+     */
+    lazy val Robot = config("robot") extend (Compile)
+  }
 
-  val RobotConfig = config("robot")
+  import autoImport._
 
-  val settings = inConfig(RobotConfig)(Seq(
-    user := NoUser,
-    tasks := Seq.empty[GenericTask]
-  ))
+  /**
+   * Adds new task to the 'Robot' configuration.
+   *
+   * @param task the task definition
+   * @param name name of the new task
+   * @param description description of the new task
+   * @param verboseLevel verbosity level of the new task
+   * @return a sbt task
+   */
+  def defineTask(task: TaskM[Boolean], name: String = "", description: String = "no description",
+                 verboseLevel: VerbosityLevel = Verbose) = {
+    //TODO check if the name already exists
+
+    (TaskKey[Option[String]](name, description) in Robot) <<= Def.task {
+      println(s"running task '$name' ($description)")
+      val (result, _, _) = task.run(verboseLevel)
+
+      result match {
+        case Success(r) if r => Option(s"task '$name' ($description) completed successfully.")
+        case _ => None
+      }
+    }
+  }
 }
